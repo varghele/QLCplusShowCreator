f47f34cf70626f860b2cc14ea5260a46a0f25373
varghele <mgm.fischer@gmail.com>
2025-03-21 13:25:17 +0100

    added error handling for formats of num_bars and bpms, modified effects ini

--- effects/__init__.py
+++ effects/__init__.py
@@ -2,12 +2,6 @@
 Effects package for light show sequences.
 This package contains different effect modules organized by fixture type.
 """
-
-#from . import bars  # Import the blinders module
-#from . import dimmers
-#from . import monocolor
-
-
 # List available effect modules
 available_effects = ['bars', 'dimmers', 'monocolor', 'moving_heads', 'multicolor']
 
@@ -19,9 +13,3 @@ DEFAULT_HOLD = 0
 def get_available_effects():
     """Returns list of available effect modules"""
     return available_effects
-
-# As you add more effect modules (like spots.py, bars.py),
-# add them to both the imports and available_effects list:
-# from . import spots
-# from . import bars
-# available_effects = ['blinders', 'spots', 'bars']
