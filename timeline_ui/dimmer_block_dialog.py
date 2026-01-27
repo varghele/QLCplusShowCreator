eca2dcf1c4bb0bf3ab0767ae827445eda914e561
varghele <mgm.fischer@gmail.com>
2026-01-27 11:44:21 +0100

    added more speed presets

--- timeline_ui/dimmer_block_dialog.py
+++ timeline_ui/dimmer_block_dialog.py
@@ -83,7 +83,7 @@ class DimmerBlockDialog(QDialog):
 
         # Effect speed selector
         self.effect_speed_combo = QComboBox()
-        self.effect_speed_combo.addItems(["1/4", "1/2", "1", "2", "4"])
+        self.effect_speed_combo.addItems(["1/16", "1/8", "1/4", "1/2", "1", "2", "4", "8", "16"])
         self.effect_speed_combo.setCurrentText("1")
         effect_layout.addRow("Speed:", self.effect_speed_combo)
 
